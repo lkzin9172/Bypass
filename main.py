b1f392fcd85f79d726c527d87897754883993149
lkzin9172 <kaiqueisac6@gmail.com>
2026-09-24 20:32:39 -0300

    Fix syntax error in app.run call

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ def index():
     return "OK"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',10000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
     
